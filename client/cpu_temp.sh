8d8c1bed6f7898bd200d947559095c6b32a40f74
Etherion <19214485+jjziets@users.noreply.github.com>
2023-03-23 16:09:00 +0200

    Update cpu_temp.sh

--- client/cpu_temp.sh
+++ client/cpu_temp.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Get the number of CPUs
-CPU_COUNT=$(sensors | grep -Ec '^Package id [0-9]+:')
+CPU_COUNT=$(sensors | grep -c "Package id")
 
-for CPU_INDEX in $(seq 0 $(($CPU_COUNT - 1))); do
-  # Get the temperature for the current CPU
-  CPU_TEMP=$(sensors | grep -oP "Package id $CPU_INDEX:\s+\+\K[\d.]+")
+for p in $(seq 0 "$((CPU_COUNT - 1))"); do
+  CPU_TEMP=$(sensors | grep -oP "Package id $p:\s+\+\K[\d.]+")
 
-  # Output the metric for the current CPU
-  echo "node_cpu_${CPU_INDEX}_temperature_celsius ${CPU_TEMP}"
+  echo "node_cpu_temperature{package=\"$p\"} $CPU_TEMP"
 done > /var/lib/node_exporter/textfile_collector/cpu_temp.prom
