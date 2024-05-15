7abb9dfb7485cd6dd481a8035cade5887d31b434
Etherion <19214485+jjziets@users.noreply.github.com>
2024-05-15 22:22:42 +0200

    Update install_NvidiaDCGM_Exporter.sh

--- client/install_NvidiaDCGM_Exporter.sh
+++ client/install_NvidiaDCGM_Exporter.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+rm /etc/apt/sources.list.d/archive_uri-https_developer_download_nvidia_com_compute_cuda_repos_ubuntu2204_x86_64_-jammy.list
+rm /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list
+rm /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list.distUpgrade
+rm /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list.save
+rm /etc/apt/sources.list.d/cuda.list
+
 set -e
 
 # Update and install necessary packages
@@ -7,12 +13,6 @@ echo "Updating package list and installing necessary packages..."
 apt update
 apt install -y git wget lsb-release software-properties-common snapd
 export DEBIAN_FRONTEND=noninteractive
-
-#rm /etc/apt/sources.list.d/archive_uri-https_developer_download_nvidia_com_compute_cuda_repos_ubuntu2204_x86_64_-jammy.list
-rm /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list
-rm /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list.distUpgrade
-rm /etc/apt/sources.list.d/cuda-ubuntu2004-x86_64.list.save
-rm /etc/apt/sources.list.d/cuda.list
 
 
 
